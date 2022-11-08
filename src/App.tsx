67f51cfa58b59d962d465de7edf1f5afac194d55
Alaya95 <rozetta.95@mail.ru>
2022-11-09 01:06:38 +0300

    edited app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import './app/index.scss';
+
+import { Header } from "./widgets/Header/Header";
+import {Footer} from "./widgets/Footer/Footer";
+import {News} from "./pages/News/News";
 
 function App() {
   return (
-    <div id="app" className="App"></div>
+    <div id="app" className="app">
+        <div className="wrapper">
+            <div className="content">
+                <Header />
+                <News />
+            </div>
+            <Footer />
+        </div>
+
+    </div>
   );
 }
 
