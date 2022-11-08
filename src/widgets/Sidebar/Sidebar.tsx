744a162736db8fd9a76a5a487cc97f458622b386
Alaya95 <rozetta.95@mail.ru>
2022-11-09 00:19:33 +0300

    added empty components for further development and styles for it

---
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Sidebar = () => {
+    return (
+        <select name="" id="">
+            <option value="" id=''></option>
+        </select>
+    );
+}
