06ae73aa88ee70e79ffa7fe3aef3fdea4386ad8b
Alaya95 <rozetta.95@mail.ru>
2022-11-09 00:16:07 +0300

    added the News Item component, types and styles for it

---
+++ src/features/NewsItem/NewsItemTypes.ts
@@ -0,0 +1,12 @@
+
+export interface NewsItemTypes {
+    data: {
+        id: number
+        imgUrl?: string
+        title: string
+        desc: string
+        date: string
+        link: string
+    }
+    className?: string
+}
