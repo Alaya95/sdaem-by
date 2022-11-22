07c79823346820e599825cd3b01a2fb1c59104a1
Alaya95 <rozetta.95@mail.ru>
2022-11-22 20:41:13 +0300

    Ui button component.

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { FC } from "react";
+import styles from "./Button.module.scss";
+import { ButtonProps } from "./ButtonTypes";
+import cn from "classnames";
 
-export const Button = () => {
-    return (
-        <div></div>
-    );
-}
+export const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  colors = "inherit",
+  variants = "outlined",
+  size = "medium",
+  ...otherProps
+}) => {
+  return (
+    <button
+      className={cn(
+        styles.base, styles[colors],
+        styles[size], styles[variants],
+        className
+      )}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
