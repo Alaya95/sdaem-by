d667ef068cbe22d6bb3a0c4055c407f36b7decb6
Alaya95 <rozetta.95@mail.ru>
2022-11-22 20:41:37 +0300

    edited pagination component

--- src/widgets/Pagination/Pagination.tsx
+++ src/widgets/Pagination/Pagination.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import cls from './pagination.module.scss';
+import classNames from "classnames";
 
 
 export const Pagination = () => {
     return (
         <div className={cls.pagination}>
-            <a className={cls.pagination__link} href="/">1</a>
+            <a className={classNames(cls.pagination__link, cls.active)} href="/">1</a>
             <a className={cls.pagination__link} href="/">2</a>
             <a className={cls.pagination__link} href="/">3</a>
             <a className={cls.pagination__link} href="/">4</a>
