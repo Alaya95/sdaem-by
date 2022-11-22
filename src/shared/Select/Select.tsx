8c8237890c6798a3808843fad9e956a0662f9bb9
Alaya95 <rozetta.95@mail.ru>
2022-11-22 20:41:55 +0300

    edited select component

--- src/shared/Select/Select.tsx
+++ src/shared/Select/Select.tsx
@@ -1,10 +1,11 @@
 import React, {FC} from 'react';
 import {SelectPropsTypes} from "./SelectTypes";
 import cls from './select.module.scss';
+import classNames from "classnames";
 
-export const Select:FC <SelectPropsTypes> = ({ dataArr, name }) => {
+export const Select:FC <SelectPropsTypes> = ({ dataArr, name, className }) => {
     return (
-        <select className={cls.select} name={name}>
+        <select className={classNames(cls.select, className)}  name={name}>
             <option hidden selected disabled>Выбрать</option>
             {dataArr.map(({id, value, name}) => {
                 return (
