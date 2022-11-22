b79c4048288d98e2365485744acee0a2ce3a7e3b
Alaya95 <rozetta.95@mail.ru>
2022-11-22 20:46:57 +0300

    added data for catalog pages

---
+++ src/pages/Catalog/data.js
@@ -0,0 +1,28 @@
+export const recommendApartments = [
+  { id: "1", name: "Недорогие" },
+  { id: "2", name: "1-комнатные" },
+  { id: "3", name: "2-комнатные" },
+  { id: "4", name: "3-комнатные" },
+  { id: "5", name: "4-комнатные" },
+  { id: "6", name: "5-комнатные" },
+  { id: "7", name: "Заводской р." },
+  { id: "8", name: "Ленинский р." },
+  { id: "9", name: "Московский р." },
+  { id: "10", name: "Октябрьский р." },
+  { id: "11", name: "Партизанский р." },
+  { id: "12", name: "Первомайский р." },
+  { id: "13", name: "Советский р." },
+  { id: "14", name: "Фрунзенский р." },
+  { id: "15", name: "Центральный р." },
+];
+export const catalogFilter = [
+  { id: 1, value: "1-комнатные", name: "1-комнатные" },
+  { id: 2, value: "2-комнатные", name: "2-комнатные" },
+  { id: 3, value: "3-комнатные", name: "3-комнатные" },
+  { id: 4, value: "4-комнатные", name: "4-комнатные" },
+  { id: 5, value: "5-комнатные", name: "5-комнатные" },
+];
+export const catalogSortItems = [
+  { id: 1, value: "priceMin", name: "дешевле" },
+  { id: 2, value: "priceMax", name: "дороже" },
+];
