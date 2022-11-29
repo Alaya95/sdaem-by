0b6c5b235f24d2ace676563d998ac52c47a93298
Alaya95 <rozetta.95@mail.ru>
2022-11-29 20:37:41 +0300

    connecting the store

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {setupStore} from "./app/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,8 +11,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+      <Provider store={setupStore()} >
       <BrowserRouter>
           <App />
       </BrowserRouter>
+  </Provider>
   </React.StrictMode>
 );
