6caf12f8d7856938c4d83edb57b720fa68eaf942
Alaya95 <rozetta.95@mail.ru>
2022-12-06 22:29:12 +0300

    removed cities reducers, actions, selectors, types.

--- src/entities/cities/selectors.ts
+++
@@ -1,6 +0,0 @@
-import {RootState} from "../../app/store";
-import {Icity} from "./citiesTypes";
-
-export const getCitiesSelector = (state: RootState): Icity => state.cities.cities;
-export const getCitiesErrorSelector = (state: RootState): string | null => state.cities.error;
-export const getIsCitiesLoadingSelector = (state: RootState): boolean => state.cities.isLoading;
