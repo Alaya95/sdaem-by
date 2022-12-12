523ecc3d290c70a78793dd5d9ccbbcb08b645104
Alaya95 <rozetta.95@mail.ru>
2022-12-12 11:16:53 +0300

    Store. catalog. middlewars, rootReducer. added catalog model.

--- src/app/store/middleware.ts
+++ src/app/store/middleware.ts
@@ -1,7 +1,9 @@
 import {citiesModel} from "../../entities/cities";
 import {newsModel} from "../../entities/news";
+import {catalogModel} from "../../entities/catalog";
 
 export const middlewareStore = [
     citiesModel.citiesApi.middleware,
-    newsModel.newsApi.middleware
+    newsModel.newsApi.middleware,
+    catalogModel.catalogApi.middleware
 ]
--- src/app/store/rootReducer.ts
+++ src/app/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from "@reduxjs/toolkit";
 import {citiesModel} from "../../entities/cities";
 import {newsModel} from "../../entities/news";
+import {catalogModel} from "../../entities/catalog";
 
 export const rootReducer = combineReducers({
     [citiesModel.citiesApi.reducerPath]: citiesModel.citiesApi.reducer,
     [newsModel.newsApi.reducerPath]: newsModel.newsApi.reducer,
+    [catalogModel.catalogApi.reducerPath]: catalogModel.catalogApi.reducer
 });
